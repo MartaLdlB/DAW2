c3b8c177156633d102d6f95ab9054cf1fbb6b4cc
MartaLdlB <marta.lopez.delosbueis@gmail.com>
2024-10-23 10:08:35 +0200

    Delete Desarrollo_entorno_servidor directory

--- Desarrollo_entorno_servidor/U3/Tarea_3_2_Marta_Lopez_de_los_Bueis/login_basico.php
+++
@@ -1,4 +0,0 @@
-<?php
-    echo "Usuario introducido: " . $_POST["usuario"];
-    echo "<br>";
-    echo "Clave introducida: " . $_POST["pw"];
--- Desarrollo_entorno_servidor/U3/otras tareas/sesiones_uso_basico2.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-session_start();
-
-echo "Variable contador= ".$_SESSION['contador'];
