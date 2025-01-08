bab5be70a48ce4e4f307ad45b2a86668098d39d1
MartaLdlB <marta.lopez.delosbueis@gmail.com>
2025-01-08 10:12:46 +0100

    nueva tarea

---
+++ Desarrollo_entorno_servidor/U7/ejspring1/src/main/java/es/daw2/ejspring1/Ejspring1Application.java
@@ -0,0 +1,13 @@
+package es.daw2.ejspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //anotación. al usar palabras reservadas, utiliza lo que se necesite para funcionar en funcion a lo que se le pida
+public class Ejspring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejspring1Application.class, args);
+	}
+
+}
