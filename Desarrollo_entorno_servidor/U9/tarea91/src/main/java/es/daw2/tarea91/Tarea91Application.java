57bb9f1548d7b709de4fa1545f056219547adb55
MartaLdlB <marta.lopez.delosbueis@gmail.com>
2025-01-27 13:23:12 +0100

    actualizacion de ejercicios

---
+++ Desarrollo_entorno_servidor/U9/tarea91/src/main/java/es/daw2/tarea91/Tarea91Application.java
@@ -0,0 +1,13 @@
+package es.daw2.tarea91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea91Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea91Application.class, args);
+	}
+
+}
