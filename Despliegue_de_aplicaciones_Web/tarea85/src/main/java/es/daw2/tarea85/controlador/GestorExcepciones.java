3941785d44c7488864061c3f000f4bc9287269bf
MartaLdlB <marta.lopez.delosbueis@gmail.com>
2025-02-14 10:19:11 +0100

    actualizar

---
+++ Despliegue_de_aplicaciones_Web/tarea85/src/main/java/es/daw2/tarea85/controlador/GestorExcepciones.java
@@ -0,0 +1,14 @@
+package es.daw2.tarea85.controlador;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GestorExcepciones {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException exception) {
+        return ResponseEntity.badRequest().body("El índice n no es válido");
+    }
+}
