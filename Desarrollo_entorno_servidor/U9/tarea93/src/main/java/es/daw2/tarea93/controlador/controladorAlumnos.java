0a4a9be7932e556b26a62a9627eafefcac427063
MartaLdlB <marta.lopez.delosbueis@gmail.com>
2025-02-23 11:58:01 +0100

    .

--- Desarrollo_entorno_servidor/U9/tarea93/src/main/java/es/daw2/tarea93/controlador/controladorAlumnos.java
+++ Desarrollo_entorno_servidor/U9/tarea93/src/main/java/es/daw2/tarea93/controlador/controladorAlumnos.java
@@ -3,7 +3,6 @@ package es.daw2.tarea93.controlador;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.daw2.tarea93.modelo.Alumno;
-import es.daw2.tarea93.modelo.Grupo;
 import es.daw2.tarea93.servicio.ServicioAlumno;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.net.URI;
 import java.util.Optional;
